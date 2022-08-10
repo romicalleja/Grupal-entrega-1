adb20485eefd952e8b3347528882475dfe8ce37d
Gonzalo Ferreira <gonzqwe@gmail.com>
2022-08-10 18:52:10 -0300

    Update validacion.js

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,6 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+function ValidarRegistro(){
+    if(document.getElementById('terminos').checked)
+}
