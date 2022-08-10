35c7929674f189f05d9888780912ec36524584b6
romicalleja <romycalleja.12.08@gmail.com>
2022-08-10 19:14:08 -0300

    Update validacion.js

--- js/validacion.js
+++ js/validacion.js
@@ -8,11 +8,11 @@ function showAlertError() {
 var password1 = document.getElementById("password1")
       , Password2 = document.getElementById("confirm2");
 function ValidarRegistro(){
-    if (password1.value!=Password2.value 
+    if (password1.value!=Password2.value) 
         return showAlertError()
         else if
-        document.getElementById('terminos').checked===false)
-        return showAlertError
+        (document.getElementById('terminos').checked===false)
+        return showAlertError()
         else
-        showAlertSuccess          
+        showAlertSuccess()          
     }
