6f41ef87a5eff254a5993875c19888d840d728a5
romicalleja <romycalleja.12.08@gmail.com>
2022-08-10 18:53:52 -0300

    password

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,10 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+var password1 = document.getElementById("password1")
+      , Password2 = document.getElementById("confirm2");
+function contrasena(){
+    if (password1.value=Password2.value )
+    
+
+}
